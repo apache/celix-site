a20645534ec7401ed1c6c8f04cafed937d2bdde1
Roy Lenferink <lenferinkroy@gmail.com>
2021-04-18 14:19:30 +0200

    Remove generated Doxygen docs from git

--- static/docs/2.2.1/html/search/all_9.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['readme_2emd_161',['README.md',['../README_8md.html',1,'']]],
-  ['remove_162',['remove',['../structcelix__service__tracking__options.html#a8445caaae53d71a47c0289d2e3e5f882',1,'celix_service_tracking_options::remove()'],['../structcelix__dm__service__dependency__callback__options.html#a10fe45460fc65e1590a58477f515424b',1,'celix_dm_service_dependency_callback_options::remove()']]],
-  ['removewithowner_163',['removeWithOwner',['../structcelix__service__tracking__options.html#acf6dbcde52efbed145933e8a28f3f2e1',1,'celix_service_tracking_options']]],
-  ['removewithproperties_164',['removeWithProperties',['../structcelix__service__tracking__options.html#af75c423757dcd16bf90d57a5726ac98a',1,'celix_service_tracking_options']]],
-  ['removewithprops_165',['removeWithProps',['../structcelix__dm__service__dependency__callback__options.html#a54ee61c8ab040dc87de22588013316fd',1,'celix_dm_service_dependency_callback_options']]],
-  ['required_166',['required',['../structcelix__dm__service__dependency__info__struct.html#a8ad066c5fa5d75e9fb82add72cbfc705',1,'celix_dm_service_dependency_info_struct']]]
-];
